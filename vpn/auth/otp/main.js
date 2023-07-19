bea76a86b906a07dbae25607cb4d49740c744be7
José Oliveira <jose@habitushealth.net>
2023-07-19 14:50:13 +0100

    added support for google authenticator

--- vpn/auth/otp/main.js
+++ vpn/auth/otp/main.js
@@ -12,8 +12,8 @@ if (!otp)
   process.exit(1)
 
 const hmac = crypto.createHmac('SHA512', hmacSecret)
-const secret = hmac.update(uid).digest('base32').subarray(0,32)
-const encoded =  base32.stringify(secret)
+const secret = hmac.update(uid).digest('base32')
+const encoded =  base32.stringify(secret).substring(0,32)
 
 const validOtp = authenticator.check(otp, encoded)
 if (validOtp) {
