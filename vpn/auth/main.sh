f780d2a9bda6045b695a2d72b7f5d47166a6fb7b
José Oliveira <jose@habitushealth.net>
2023-08-16 14:36:37 +0100

    removed azure resource owner password authentication

--- vpn/auth/main.sh
+++ vpn/auth/main.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -eE
 set -eE
 
-auth=-1
-
 if [[ ! -z $VPN_AUTH_OTP_SECRET ]]; then
   echo "checking OTP authentication for the  user $username ..."
   export password=$(echo $original_password | cut -d ':' -f3 | base64 -d )
-  /app/vpn/auth/otp/main.js; auth=$(($auth+$?))
+  /app/vpn/auth/otp/main.js; auth=$?
 fi
 
 if [[ $auth != 0 ]]; then
